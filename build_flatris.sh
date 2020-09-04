528f41a5cc153d161104e4da4a4976122c245513
avgitpal <70725114+avgitpal@users.noreply.github.com>
2020-09-05 00:09:49 +0300

    Update build_flatris.sh

--- build_flatris.sh
+++ build_flatris.sh
@@ -5,8 +5,6 @@ cp Dockerfile ./flatris
 cp docker-compose.yaml ./flatris
 cd flatris/
 docker build . -t flatris
-#docker run -p 3000:3000 -it flatris
-
 
 docker-compose build
 docker-compose up -d
